f0b278cc53aae01bf4de8ce590cf1c689bfcdb3c
aupamwantle24-debug <aupamwantle24@gmail.com>
2026-04-16 15:19:03 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Discover Botswana 🇧🇼</h1>
+
+      <h2>Kasane</h2>
+      <p>Gateway to Chobe National Park.</p>
+
+      <a href="https://www.google.com/maps/search/?api=1&query=Kasane+Botswana" target="_blank">
+        Navigate
+      </a>
+    </div>
+  );
+}
